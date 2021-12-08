aeff245f9c0d03aefbf4878dcc324b3244cda015
baumfalk <jtbaumfalk@gmail.com>
2021-12-08 15:27:21 +0100

    cleane oplossing 8 gevonden

--- 08/08.py
+++ 08/08.py
@@ -10,6 +10,7 @@ def run_program(inp="input.txt"):
     print(f"Running on {inp}")
     print(part1(content))
     print(part2(content))
+    print(part2_clean(content))
     print()
     
 def part1(input):
@@ -96,6 +97,34 @@ def part2(inp):
     answer = total
     return answer
 
+def part2_clean(input):
+    data = list(map(lambda s: list(zip(map(lambda y: y.split(" "),s.split(" | ")))), input))
+    res = 0
+    for a, b in data:
+        a = a[0]
+        b = b[0]
+        d = dict()
+        for word in sorted(a, key=len):
+            word = set(word)
+            l = len(word)
+            if   l == 2: d[1] = word
+            elif l == 3: d[7] = word
+            elif l == 4: d[4] = word
+            elif l == 7: d[8] = word
+            elif l == 5:
+                if   len(word & d[7]) == 3: d[3] = word
+                elif len(word & d[4]) == 3: d[5] = word
+                else:                       d[2] = word
+            elif l == 6:
+                if len(word & d[5]) == 5:
+                    if len(word & d[7]) == 2: d[6] = word
+                    else:                     d[9] = word
+                else:                         d[0] = word
+        digits = (str(k) for digit in b
+                         for k, v in d.items()
+                         if set(digit) == v)
+        res += int("".join(digits))
+    return res
 
 run_program("test_input.txt")
 run_program("input.txt")
---
+++ 09/09.py
@@ -0,0 +1,32 @@
+import numpy as np
+from collections import defaultdict
+from collections import Counter
+import networkx as nx
+import re
+
+def run_program(inp="input.txt"):
+    #diag = np.genfromtxt(inp, delimiter=1).astype(int)
+    content = list(map(lambda s: s.strip('\r\n'), open(inp).readlines()))
+    print(f"Running on {inp}")
+    print(part1(content))
+    print(part2(content))
+    print()
+    
+def part1(input):
+    #int_list = np.array(list(map(int,input[0].split(","))))
+    #input_proc = list(map(lambda s: s.split(" | "), input))
+   
+    answer = None
+    return answer
+
+def part2(input):
+    #int_list = np.array(list(map(int, input[0].split(","))))
+    #input_proc = list(map(lambda s: s.split(" | "), input))
+    
+        
+    answer = None
+    return answer
+
+
+run_program("test_input.txt")
+run_program("input.txt")
